0d33523c456de93af28541831aa984a1be38880a
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-07-31 20:45:37 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function($) {
+
+  $.ajax({
+    method: "GET",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=abbff5eb9459ab2687d318b3c2e71c9e"
+  })
+  .done(function(data) {
+    console.log(data);
+    console.log(data.name);
+    console.log(data.main.temp);
+    console.log(data.weather[0].description);
+    console.log(data.wind.speed);
+  })
+  .fail(function () {
+
+    console.log("error");
+
+  });
+
+});
