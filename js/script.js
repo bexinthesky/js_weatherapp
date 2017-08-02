b980100ffdd16fec31e9a45201f38536fd2b3888
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-08-02 07:43:36 -0700

    added font awesome

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function($) {
 
   $.ajax({
     method: "GET",
-    url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=abbff5eb9459ab2687d318b3c2e71c9e"
+    url: "http://api.openweathermap.org/data/2.5/weather?zip=93704,us&appid=abbff5eb9459ab2687d318b3c2e71c9e"
   })
   .done(function(data) {
     console.log(data);
