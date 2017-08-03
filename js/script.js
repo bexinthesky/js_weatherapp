b51d3690fe0a8cbd4ff95523caa9c616c728dde1
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-08-02 20:17:40 -0700

    all the weather javascript and slightly nicer html

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-$(document).ready(function($) {
 
+
+
+// call API info
+$(document).ready(function($) {
+  document.getElementById('submit-button').addEventListener('click', function(e) {
+  	e.preventDefault();
+      var zipCode = document.getElementById('zip-code').value;
+      console.log(zipCode);
+
+      // console.log(zipCode);
   $.ajax({
+
     method: "GET",
-    url: "http://api.openweathermap.org/data/2.5/weather?zip=93704,us&appid=abbff5eb9459ab2687d318b3c2e71c9e"
+    url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=abbff5eb9459ab2687d318b3c2e71c9e&units=metric"
   })
   .done(function(data) {
-    console.log(data);
-    console.log(data.name);
-    console.log(data.main.temp);
-    console.log(data.weather[0].description);
-    console.log(data.wind.speed);
+    $("#div-id").empty();
+
+    $('#div-id').append("<p>" + "City: " + data.name + "</p>" + "<br>" + "<p>" + "Temperature: " + data.main.temp + "</p>" + "<br>" + "<p>" + "Sky: " + data.weather[0].description + "</p>" + "<br>" + "<p>" + "Minimum Temperature: " + data.main.temp_min + "</p>" + "<br>" + "<p>" + "Maximum Temperature: " + data.main.temp_max + "</p>" + "<br>" + "<p>" + "Humidity: " + data.main.humidity + "</p>" + "<br>" + "<p>" + "Wind Speed: " + data.wind.speed + "</p>" + "<br>" + "<p>" + "Sunrise: " + data.sys.sunrise + "</p>" + "<br>" + "<p>" + "Sunset: " + data.sys.sunset + "</p>");
+
   })
   .fail(function () {
 
     console.log("error");
 
   });
-
 });
+});
+
+//.submit with var thar connects to api
+// create elements that are needed
